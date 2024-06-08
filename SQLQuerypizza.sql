545dd73dd41d5eacd58efc5401d9c12856696697
nguyen-william <77467480+nguyen-william@users.noreply.github.com>
2024-06-07 23:39:37 -0400

    more practice queries

--- SQLQuerypizza.sql
+++ SQLQuerypizza.sql
@@ -1,2 +1,6 @@
 
-SELECT SUM(total_price) AS Total_Revenue from pizza_sales
+SELECT * FROM pizza_sales
+
+SELECT DATENAME(DW, order_date) as order_day, COUNT(DISTINCT order_id) AS Total_orders 
+from pizza_sales
+GROUP BY DATENAME(DW, order_date)
